57c4bfa46b3ad2913e054ef2cda5920b94d2c932
Daniel Albuquerq <daniel_developer@hotmailcom>
2026-04-30 00:17:16 -0300

    RestaurantSchema 1.0

--- README.md
+++ README.md
@@ -16,14 +16,18 @@ Aplicativo de entrega:
 - Gerenciamento de Estado com Redux Toolkit para garantir que o carrinho persista enquanto o usuário navega entre diferentes restaurantes.
 
 3. Funcionalidades de Core (MVP)
+
 - Geofencing: Definir o raio de entrega dos restaurantes e calcular o frete com base na distância.
 - Gateway de Pagamento: Integração com APIs como Stripe, Adyen ou Pagar.me para processar cartões e Pix.
 - Sistema de Order Flow: O estado do pedido: Pendente → Em Preparo → Saiu para Entrega → Entregue.
 - Push Notifications: Essencial para manter o usuário e o entregador engajados no processo.
 
 4. Primeiro Passos Práticos
+
 - Modelagem de Dados: Comecei desenhando o esquema do banco de dados (Usuários, Produtos, Pedidos, Endereços).
 - Desenvolvimento do Backend: Criei as rotas de autenticação (JWT) e o CRUD de produtos. 
 - Integração de Mapas: Configurei com a-APIs externas para lidar com endereços e rotas.
 
 Arquitetura MERN para Delivery
+
+- Mongoose Schema: Opetei por taxa de entrega fixa pois a cidade de Luís Gomes é pequena, utilizarei o sistema de coordenadas 2dsphere do MongoDB
---
+++ server/src/models/Restaurant.js
@@ -0,0 +1,55 @@
+import mongoose, { Schema } from "mongoose"
+
+const RestaurantSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+        trim: true 
+    },
+    logo: {
+        type: String, //URL da imagem (S3 ou Cloudinary)
+        required: true
+    },
+    categoria:{
+        type: String, 
+        enum: ['Lanches', 'Japonesa', 'Pizza', 'Brasileira', 'Doces'],
+        required: true
+    },
+    //Configuração de Entrega
+    taxaEntrega: {
+        type: Number, 
+        required: true,
+        default: 0 //Valor fixo em centavos (ex: 700 para 7,00)
+    },
+    raioEntregaKm: {
+        type: Number,
+        required: true,
+        default: 5 //Raio máximo de atendimento
+    },
+    // Localização para o MongoDB Geospatial
+    localização: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            default: 'Point'
+        },
+        coordinates: {
+            type: [Number], //[longitude, latitude]
+            required: true,
+        }, 
+    },
+    tempoEntregaEstimado:{
+        type: String, // Ex: "30-40"
+        default: "30 min"
+    },
+    ativo:{
+        type: Boolean,
+        default: true
+    }
+}, {timestamps: true});
+
+// Criação do índice para buscas por proximidade
+RestaurantSchema.index({ localizacao: '2dsphere' });
+
+const Restaurant = mongoose.model("Restaurant", RestaurantSchema)
+export default Restaurant
